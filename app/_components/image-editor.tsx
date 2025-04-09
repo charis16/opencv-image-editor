92a491d04cc9e039358916201132a3ea9e601abf
charisredcomm <muhammad.charis@redcomm.co.id>
2025-04-09 16:57:00 +0700

    ref: change name

--- app/_components/image-editor.tsx
+++ app/_components/image-editor.tsx
@@ -37,7 +37,7 @@ export default function ImageEditor() {
   useEffect(() => {
     const loadOpenCv = async () => {
       const script = document.createElement("script");
-      script.src = "/libs/openCV.js";
+      script.src = "/libs/openCV.jsx";
       script.async = true;
       script.onload = () => {
         console.log("OpenCV loaded");
